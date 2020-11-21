efb34498d0b5d828c1555ca0f68f2bde0561da0a
alirni <aliramezani19.1@gmail.com>
2020-11-21 20:19:42 +0330

    add new btn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [hangover, setHangover] = useState(0);
 
   const handleCounter = () => {
-    console.log('App.js:8 handleCounter');
+    setCounter(counter + 1);
+    console.log('App.js:10 handleCounter');
+  }
+
+  const handleHangover = () => {
+    const start = Date.now();
+
+    while (Date.now() < start + 5000) {
+    }
+    setHangover(hangover + 1);
+    console.log('App.js:18 handleHangover');
   }
 
   return (
@@ -13,7 +25,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>counter: {counter}</code>
+        </p>
+        <p>
+          <code>hangover: {hangover}</code>
         </p>
         <a
           className="App-link"
@@ -22,7 +37,10 @@ function App() {
           rel="noopener noreferrer"
         >
         </a>
-        <div className="counterBtn" onClick={handleCounter}>COUNTER</div>
+        <div className="container">
+          <div className="counterBtn" onClick={handleCounter}>COUNTER</div>
+          <div className="counterBtn" onClick={handleHangover}>HANGOVER</div>
+        </div>
       </header>
     </div>
   );
